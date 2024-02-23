e444f76be9b75b4fc51233080b051fb55d18c15c
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-02-23 17:29:52 -0600

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
 if __name__=='__name__':
     app.config.from_object(config['development'])
