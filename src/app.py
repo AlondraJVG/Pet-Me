f13a6726e744379637b3e9ffbea53d05ffd04ae8
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-02-23 18:26:48 -0600

    3.5

--- src/app.py
+++ src/app.py
@@ -1,23 +1,20 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
-# Creamos una instancia de la aplicación Flask
+from index import index
 app = Flask(__name__)
 
-# Definimos la ruta para la página de inicio
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Definimos la ruta para la segunda página
 @app.route('/pagina2')
 def pagina2():
     return render_template('pagina2.html')
 
-# Definimos una ruta para redireccionar al índice
 @app.route('/regresar_al_index')
 def regresar_al_index():
     return redirect(url_for('index'))
 
-# Si este archivo es ejecutado directamente, arrancamos el servidor
 if __name__ == '__main__':
     app.run(debug=True)
