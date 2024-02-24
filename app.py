6a3b9e3eb46007961514fca28d0985d86b99bb7e
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-02-23 18:30:47 -0600

    3.5

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/pagina2')
-def pagina2():
-    return render_template('pagina2.html')
-
-@app.route('/regresar_al_index')
-def regresar_al_index():
-    return redirect(url_for('index'))
-
 if __name__ == '__main__':
     app.run(debug=True)
+ 
