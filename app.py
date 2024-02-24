0daac6aaf97c072a54217fe3cf8faf2a13764911
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-02-23 18:43:26 -0600

    11

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from flask import Flask
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route('/')
-def index():
-    """Ruta para la página de inicio."""
-    return render_template('index.html')
+@app.route("/")
+def prueba1():
+    return "1"
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=='__name__':
+    app.run()
