67fd37533d8b8dc2593bd5c34ce58ae80cbddd00
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-03-26 16:57:05 -0600

    inventario4

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_orator import Orator
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database import Producto, Base
 
 ORATOR_DATABASES = {
     'development': {
@@ -30,33 +27,13 @@ def login():
     user = db.table("usuarios").where("user", username).where("password", password).first()
 
     if user is not None:
-        return redirect(url_for('inventario'))  # Redirige al administrador al inventario
+        return redirect(url_for('administrador'))
     else:
         return 'Nombre de usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.'
 
-@app.route('/inventario')
-def inventario():
-    main()  # Realiza operaciones en la base de datos
-    return '¡Bienvenido al inventario!'  # Muestra la página de inventario
-
-def main():
-    engine = create_engine('sqlite:///veterinaria_inventario.db', echo=True)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Agregar un nuevo producto
-    nuevo_producto = Producto(nombre="Alimento para gatos", tipo="Alimento", descripcion="Alimento balanceado para gatos adultos", precio=15.99, cantidad_stock=100)
-    session.add(nuevo_producto)
-    session.commit()
-
-    # Consultar todos los productos
-    productos = session.query(Producto).all()
-    for producto in productos:
-        print(f"ID: {producto.id}, Nombre: {producto.nombre}, Tipo: {producto.tipo}, Precio: {producto.precio}, Stock: {producto.cantidad_stock}")
-
-    session.close()
+@app.route('/administrador')
+def administrador():
+    return '¡Bienvenido al panel de administración!'
 
 if __name__ == '__main__':
     app.run(debug=True)
