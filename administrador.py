b7afe1c379c62b8f49e21818a5e87eb63742ca5d
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-03-28 00:37:53 -0600

    inventario29

--- administrador.py
+++ administrador.py
@@ -1,32 +1,28 @@
 from flask import Flask, render_template, request, jsonify
-from orator import DatabaseManager, Model
+from orator import Model, DatabaseManager
 
-config = {
+app = Flask(__name__)
+
+ORATOR_DATABASES = {
     'mysql': {
         'driver': 'mysql',
         'host': 'petClinical.mysql.pythonanywhere-services.com',
         'database': 'petClinical$default',
         'user': 'petClinical',
-        'password': 'Alondra.77',
-        'prefix': ''
+        'password': 'Alondra.77'
     }
 }
 
-db = DatabaseManager(config)
+db = DatabaseManager(ORATOR_DATABASES)
 Model.set_connection_resolver(db)
 
 class Producto(Model):
-    __fillable__ = ['nombre', 'tipo', 'descripcion', 'precio', 'cantidad_stock']
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    productos = Producto.all()
-    return render_template('administrador.html', productos=productos)
+    __table__ = 'Productos'
+    __timestamps__ = False
 
 @app.route('/agregar_producto', methods=['POST'])
 def agregar_producto():
+
     nombre = request.form['nombre']
     tipo = request.form['tipo']
     descripcion = request.form['descripcion']
@@ -41,6 +37,11 @@ def agregar_producto():
                        'cantidad_stock': producto.cantidad_stock} for producto in productos]
 
     return jsonify(productos=productos_json)
+
+@app.route('/')
+def administrador():
+    productos = Producto.all()
+    return render_template('administrador.html', productos=productos)
 
 if __name__ == '__main__':
     app.run(debug=True)
