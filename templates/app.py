3a8c4c258a6aa000bd6d59ddd66f7e4f7dfc882a
AlondxraVG <zap0460@zapopan.tecmm.edu.mx>
2024-03-28 00:05:52 -0600

    inventario28

--- templates/app.py
+++ templates/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_orator import Orator
+from werkzeug.security import check_password_hash
 
 ORATOR_DATABASES = {
     'development': {
@@ -12,9 +13,11 @@ ORATOR_DATABASES = {
 }
 
 app = Flask(__name__)
-
 app.config['ORATOR_DATABASES'] = ORATOR_DATABASES
 db = Orator(app)
+
+class Usuario(db.Model):
+    __table__ = 'usuarios'
 
 @app.route('/')
 def index():
@@ -24,12 +27,12 @@ def index():
 def login():
     username = request.form['username']
     password = request.form['password']
-    user = db.table("usuarios").where("user", username).where("password", password).first()
+    user = Usuario.where("user", username).first()
 
-    if user is not None:
+    if user and check_password_hash(user.password, password):
         return redirect(url_for('administrador'))
     else:
-        return 'Nombre de usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.'
+        return 'Nombre de usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.', 401
 
 @app.route('/administrador')
 def administrador():
